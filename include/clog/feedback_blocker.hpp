6c414b5303a517f76435777e6bdef5f9dbae9a68
Colugo <68328892+colugomusic@users.noreply.github.com>
2022-11-25 12:28:35 +0000

    Create feedback_blocker.hpp

---
+++ include/clog/feedback_blocker.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+namespace clog {
+
+struct feedback_blocker
+{
+	struct scope
+	{
+		scope(bool* flag)
+			: flag_{flag}
+		{
+			*flag_ = true;
+		}
+
+		scope(scope&& rhs) noexcept
+			: flag_{rhs.flag_}
+		{
+			rhs.flag_ = {};
+		}
+
+		auto operator=(scope&& rhs) noexcept -> scope&
+		{
+			flag_ = rhs.flag_;
+			rhs.flag_ = {};
+			return *this;
+		}
+
+		~scope()
+		{
+			if (flag_)
+			{
+				*flag_ = false;
+			}
+		}
+
+		scope(const scope& rhs) = delete;
+		auto operator=(const scope& rhs) noexcept -> scope& = delete;
+
+	private:
+
+		bool* flag_;
+	};
+
+	auto operator()() -> scope
+	{
+		return {&flag_};
+	}
+
+	operator bool() const
+	{
+		return flag_;
+	}
+
+private:
+
+	bool flag_{false};
+};
+
+} // clog
