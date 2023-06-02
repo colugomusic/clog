f0f4ce7a1f2194621ea34d2ec5b2f2d8576efd7a
Colugo <68328892+colugomusic@users.noreply.github.com>
2023-06-02 16:15:13 +0100

    Update auto_array.hpp

--- include/clog/auto_array.hpp
+++ include/clog/auto_array.hpp
@@ -3,7 +3,11 @@
 namespace clg {
 template <typename T>
 struct auto_array : private std::vector<T> {
+	using std::vector<T>::clear;
 	using size_type = typename std::vector<T>::size_type;
+	auto at(size_type pos) const -> const T& {
+		return std::vector<T>::at(pos);
+	}
 	auto operator[](size_type pos) -> T& {
 		if (pos >= size()) {
 			resize(pos+1);
