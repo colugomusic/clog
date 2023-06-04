b9be7aa1c2241a5fbad73af54aca82cdc32f947c
Colugo <68328892+colugomusic@users.noreply.github.com>
2023-06-04 09:54:35 +0100

    Update auto_array.hpp

--- include/clog/auto_array.hpp
+++ include/clog/auto_array.hpp
@@ -3,8 +3,20 @@
 namespace clg {
 template <typename T>
 struct auto_array : private std::vector<T> {
+	using std::vector<T>::begin;
+	using std::vector<T>::cbegin;
+	using std::vector<T>::cend;
 	using std::vector<T>::clear;
+	using std::vector<T>::crbegin;
+	using std::vector<T>::crend;
+	using std::vector<T>::end;
+	using std::vector<T>::rbegin;
+	using std::vector<T>::rend;
+	using std::vector<T>::size;
 	using size_type = typename std::vector<T>::size_type;
+	auto at(size_type pos) -> T& {
+		return std::vector<T>::at(pos);
+	}
 	auto at(size_type pos) const -> const T& {
 		return std::vector<T>::at(pos);
 	}
