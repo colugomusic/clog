0706c408707af7f148ca8a539ff899f7cf36f890
Colugo <68328892+colugomusic@users.noreply.github.com>
2023-06-04 13:39:39 +0100

    Update auto_array.hpp

--- include/clog/auto_array.hpp
+++ include/clog/auto_array.hpp
@@ -2,29 +2,19 @@
 #include <vector>
 namespace clg {
 template <typename T>
-struct auto_array : private std::vector<T> {
-	using std::vector<T>::begin;
-	using std::vector<T>::cbegin;
-	using std::vector<T>::cend;
-	using std::vector<T>::clear;
-	using std::vector<T>::crbegin;
-	using std::vector<T>::crend;
-	using std::vector<T>::end;
-	using std::vector<T>::rbegin;
-	using std::vector<T>::rend;
-	using std::vector<T>::size;
-	using size_type = typename std::vector<T>::size_type;
-	auto at(size_type pos) -> T& {
-		return std::vector<T>::at(pos);
-	}
-	auto at(size_type pos) const -> const T& {
-		return std::vector<T>::at(pos);
-	}
-	auto operator[](size_type pos) -> T& {
+struct auto_array : public std::vector<T> {
+	auto operator[](typename std::vector<T>::size_type pos) -> T& {
 		if (pos >= size()) {
 			std::vector<T>::resize(pos+1);
 		}
 		return std::vector<T>::operator[](pos);
 	}
+private:
+	using std::vector<T>::emplace_back;
+	using std::vector<T>::erase;
+	using std::vector<T>::insert;
+	using std::vector<T>::pop_back;
+	using std::vector<T>::push_back;
+	using std::vector<T>::resize;
 };
 } // clg
