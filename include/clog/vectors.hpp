b094f40dc27a61e903dea073b9487c1fd12285d3
Colugo <colugomusic@gmail.com>
2024-03-12 18:13:20 +0000

    Update vectors.hpp

--- include/clog/vectors.hpp
+++ include/clog/vectors.hpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <cassert>
+#include <iterator>
 #include <vector>
 
 #ifndef CLOG_ASSERT
@@ -22,15 +23,12 @@ namespace sorted {
 // Check if the sorted vector contains the value.
 // Precondition: The vector is sorted.
 template <typename Begin, typename End, typename T, typename Compare = std::less<T>>
-auto contains(Begin begin, End end, const T& value, Compare compare = Compare{}) -> bool
-{
-	CLOG_EXPENSIVE_ASSERT (std::is_sorted(begin, end, compare));
-
+auto contains(Begin begin, End end, const T& value, Compare compare = Compare{}) -> bool {
+	CLOG_EXPENSIVE_ASSERT (std::is_sorted(begin, end, compare)); 
 	return std::binary_search(begin, end, value, compare);
 }
 template <typename T, typename Compare = std::less<T>>
-auto contains(const std::vector<T>& vector, const T& value, Compare compare = Compare{}) -> bool
-{
+auto contains(const std::vector<T>& vector, const T& value, Compare compare = Compare{}) -> bool {
 	return contains(std::cbegin(vector), std::cend(vector), value, compare);
 }
 
@@ -38,43 +36,32 @@ auto contains(const std::vector<T>& vector, const T& value, Compare compare = Co
 // Precondition: The vector is sorted.
 // Returns: The number of elements erased.
 template <typename T, typename Compare = std::less<T>>
-auto erase_all(std::vector<T>* vector, const T& value, Compare compare = Compare{})
-{
-	CLOG_EXPENSIVE_ASSERT (std::is_sorted(std::cbegin(*vector), std::cend(*vector), compare));
-
-	const auto [beg, end] = std::equal_range(std::cbegin(*vector), std::cend(*vector), value, compare);
-
-	const auto count { std::distance(beg, end) };
-
-	vector->erase(beg, end);
-
+auto erase_all(std::vector<T>* vector, const T& value, Compare compare = Compare{}) {
+	CLOG_EXPENSIVE_ASSERT (std::is_sorted(std::cbegin(*vector), std::cend(*vector), compare)); 
+	const auto [beg, end] = std::equal_range(std::cbegin(*vector), std::cend(*vector), value, compare); 
+	const auto count { std::distance(beg, end) }; 
+	vector->erase(beg, end); 
 	return count;
 }
 
 // Return an iterator pointing to the first element equal to value, or end if not found.
 // Precondition: The range is sorted.
 template <typename Begin, typename End, typename T, typename Compare = std::less<T>>
-auto find(Begin begin, End end, const T& value, Compare compare = Compare{})
-{
-	CLOG_EXPENSIVE_ASSERT (std::is_sorted(begin, end, compare));
-
-	const auto pos { std::lower_bound(begin, end, value, compare) };
-
+auto find(Begin begin, End end, const T& value, Compare compare = Compare{}) {
+	CLOG_EXPENSIVE_ASSERT (std::is_sorted(begin, end, compare)); 
+	const auto pos { std::lower_bound(begin, end, value, compare) }; 
 	if (pos == end) return end;
-	if (compare(value, *pos)) return end;
-
+	if (compare(value, *pos)) return end; 
 	return pos;
 }
 
 template <typename T, typename U, typename Compare = std::less<T>>
-auto find(std::vector<T>& vector, const U& value, Compare compare = Compare{})
-{
+auto find(std::vector<T>& vector, const U& value, Compare compare = Compare{}) {
 	return find(std::begin(vector), std::end(vector), value, compare);
 }
 
 template <typename T, typename U, typename Compare = std::less<T>>
-auto find(const std::vector<T>& vector, const U& value, Compare compare = Compare{})
-{
+auto find(const std::vector<T>& vector, const U& value, Compare compare = Compare{}) {
 	return find(std::cbegin(vector), std::cend(vector), value, compare);
 }
 
@@ -90,8 +77,7 @@ auto insert(std::vector<T>* vector, U&& value, Compare compare = Compare{}) -> s
 // Add the range to the vector and then sort the entire vector.
 // The vector does not need to be pre-sorted.
 template <typename T, typename Begin, typename End>
-auto insert(std::vector<T>* vector, Begin begin, End end) -> void
-{
+auto insert(std::vector<T>* vector, Begin begin, End end) -> void {
 	std::copy(begin, end, std::back_inserter(*vector));
 	std::sort(std::begin(*vector), std::end(*vector));
 }
@@ -102,19 +88,13 @@ namespace unique {
 // Fails if the value already exists.
 // Precondition: The vector is sorted.
 template <typename T, typename U, typename Compare = std::less<T>>
-auto insert(std::vector<T>* vector, U&& value, Compare compare = Compare{}) -> std::pair<typename std::vector<T>::iterator, bool>
-{
-	CLOG_EXPENSIVE_ASSERT (std::is_sorted(std::cbegin(*vector), std::cend(*vector), compare));
-
-	auto pos { std::lower_bound(std::begin(*vector), std::end(*vector), value, compare) };
-
-	if (pos != std::cend(*vector) && !compare(value, *pos))
-	{
+auto insert(std::vector<T>* vector, U&& value, Compare compare = Compare{}) -> std::pair<typename std::vector<T>::iterator, bool> {
+	CLOG_EXPENSIVE_ASSERT (std::is_sorted(std::cbegin(*vector), std::cend(*vector), compare)); 
+	auto pos = std::lower_bound(std::begin(*vector), std::end(*vector), value, compare);
+	if (pos != std::cend(*vector) && !compare(value, *pos)) {
 		return { pos, false };
-	}
-
-	pos = vector->insert(pos, std::forward<U>(value));
-
+	} 
+	pos = vector->insert(pos, std::forward<U>(value)); 
 	return { pos, true };
 }
 
@@ -123,24 +103,16 @@ auto insert(std::vector<T>* vector, U&& value, Compare compare = Compare{}) -> s
 // it is overwritten.
 // Precondition: The vector is sorted.
 template <typename T, typename U, typename Compare = std::less<T>>
-auto overwrite(std::vector<T>* vector, U&& value, Compare compare = Compare{})
-{
-	CLOG_EXPENSIVE_ASSERT (std::is_sorted(std::cbegin(*vector), std::cend(*vector), compare));
-
-	auto pos { find(*vector, value, compare) };
-
-	if (pos != std::cend(*vector))
-	{
+auto overwrite(std::vector<T>* vector, U&& value, Compare compare = Compare{}) {
+	CLOG_EXPENSIVE_ASSERT (std::is_sorted(std::cbegin(*vector), std::cend(*vector), compare)); 
+	auto pos { find(*vector, value, compare) }; 
+	if (pos != std::cend(*vector)) {
 		*pos = std::forward<U>(value);
 		return pos;
-	}
-
-	bool success;
-
-	std::tie(pos, success) = insert(vector, std::forward<U>(value), compare);
-
-	assert (success);
-
+	} 
+	bool success; 
+	std::tie(pos, success) = insert(vector, std::forward<U>(value), compare); 
+	assert (success); 
 	return pos;
 }
 
@@ -151,14 +123,10 @@ namespace checked {
 // Precondition: The value does not already exist.
 // Postcondition: The value was inserted.
 template <typename T, typename U, typename Compare = std::less<T>>
-auto insert(std::vector<T>* vector, U&& value, Compare compare = Compare{})
-{
-	CLOG_EXPENSIVE_ASSERT (std::is_sorted(std::cbegin(*vector), std::cend(*vector), compare));
-
-	const auto [pos, success] = unique::insert(vector, std::forward<U>(value), compare);
-
-	CLOG_ASSERT (success);
-
+auto insert(std::vector<T>* vector, U&& value, Compare compare = Compare{}) {
+	CLOG_EXPENSIVE_ASSERT (std::is_sorted(std::cbegin(*vector), std::cend(*vector), compare)); 
+	const auto [pos, success] = unique::insert(vector, std::forward<U>(value), compare); 
+	CLOG_ASSERT (success); 
 	return pos;
 }
 
@@ -167,46 +135,31 @@ auto insert(std::vector<T>* vector, U&& value, Compare compare = Compare{})
 // Precondition: The value exists.
 // Postcondition: Exactly one value was inserted.
 template <typename T, typename Compare = std::less<T>>
-auto erase(std::vector<T>* vector, const T& value, Compare compare = Compare{}) -> void
-{
-	CLOG_EXPENSIVE_ASSERT (std::is_sorted(std::cbegin(*vector), std::cend(*vector), compare));
-
-	const auto result = sorted::erase_all(vector, value, compare);
-
+auto erase(std::vector<T>* vector, const T& value, Compare compare = Compare{}) -> void {
+	CLOG_EXPENSIVE_ASSERT (std::is_sorted(std::cbegin(*vector), std::cend(*vector), compare)); 
+	const auto result = sorted::erase_all(vector, value, compare); 
 	CLOG_ASSERT (result == 1);
 }
 
 template <typename T, typename Compare = std::less<T>>
-struct vector : public std::vector<T>
-{
-	using base_t = std::vector<T>;
-
+struct vector : public std::vector<T> {
+	using base_t = std::vector<T>; 
 	vector() = default;
 	vector(const base_t& vec) : base_t{vec} {}
-	vector(base_t && vec) : base_t{std::move(vec)} {}
-
-	auto contains(const T& value) const -> bool
-	{
+	vector(base_t && vec) : base_t{std::move(vec)} {} 
+	auto contains(const T& value) const -> bool {
 		return clg::vectors::sorted::contains(static_cast<const base_t&>(*this), value, Compare{});
-	}
-
-	auto find(const T& value) const
-	{
+	} 
+	auto find(const T& value) const {
 		return clg::vectors::sorted::find(static_cast<const base_t&>(*this), value, Compare{});
-	}
-
-	auto insert(const T& value) -> void
-	{
+	} 
+	auto insert(const T& value) -> void {
 		checked::insert(static_cast<base_t*>(this), value, Compare{});
-	}
-
-	auto insert(T&& value) -> void
-	{
+	} 
+	auto insert(T&& value) -> void {
 		checked::insert(static_cast<base_t*>(this), std::move(value), Compare{});
-	}
-
-	auto erase(const T& value) -> void
-	{
+	} 
+	auto erase(const T& value) -> void {
 		checked::erase(this, value, Compare{});
 	}
 };
